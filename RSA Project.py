55595a23ca76d9f7f87de45cb20342bbb7d029bd
MatthewHahn73 <45131687+MatthewHahn73@users.noreply.github.com>
2019-02-23 09:56:51 -0600

    Update RSA Project.py

--- RSA Project.py
+++ RSA Project.py
@@ -1,4 +1,5 @@
 import random
+import os.path
 from math import gcd
 
 def GenerateRandom(Min, Max):
@@ -31,12 +32,10 @@ def Public_Key(P, Q):
     Cont = True
     N = P*Q
     PseudoN = ((P-1)*(Q-1))
-
     while(Cont):
         E = GeneratePseudo(GeneratePrimes(),500,1000)
         if(gcd(E,PseudoN) == 1):
             Cont = False
-
     PK = (E, N)
     return PK
 
@@ -49,7 +48,7 @@ def EGCD(A, M):
 def ModInv(E, M):
     G,X,Y = EGCD(E, M)
     if G != 1:
-        raise Exception('No modular inverse')
+        raise Exception("No modular inverse")
     D = X%M
     return D
 
@@ -71,32 +70,70 @@ def Decrypt(Private, Message):
         else:
             Characters.append(String)
             String = ""
-
     for x in range(0,len(Characters)):
         Decrypted = Decrypted + chr(pow(int(Characters[x]),d,n))
     return Decrypted
 
+def Encrypt_File(PUK, PIK, fileName):
+    File = open(fileName, "r")
+    Encryption_Data = File.readlines()
+    File.close()
+    File = open(fileName, "w")
+    File.write(Encrypt(PUK, '********') + "\n")
+    for x in range(0,len(Encryption_Data)):
+        Encryption_Data[x] = Encrypt(PUK, Encryption_Data[x])
+        File.write(Encryption_Data[x] + "\n")
+    File.close()
+    File = open("Files/Keys.txt", "w")
+    File.write("Public Key: " + str(PUK) + "\n")
+    File.write("Private key: " + str(PIK) + "\n")
+    File.close()
+    print('\tFile Encrypted Successfully')
+
+def Decrypt_File(PIK, fileName):
+    File = open(fileName, "r")
+    Encryption_Data = File.readlines()
+    File.close()
+    if not Decrypt(PIK, Encryption_Data[0][:-2]) == '*******':
+        print('\tError: Wrong key set')
+    else:
+        File = open(fileName, "w")
+        for y in range(1,len(Encryption_Data)):
+            Encryption_Data[y] = Decrypt(PIK, Encryption_Data[y][:-2])
+            File.write(Encryption_Data[y] + "\n")
+        File.close()
+        print('\tDecryption Successful')
+
 if __name__ == "__main__":
-    P = GeneratePseudo(GeneratePrimes(),0,5000)
-    Q = GeneratePseudo(GeneratePrimes(),0,5000)
-
-    print("P: " + str(P))
-    print("Q: " + str(Q))
-
-    PUK = Public_Key(P, Q)
-    E, N = PUK
-    D = ModInv(E, ((P-1)*(Q-1)))
-    PIK = D, N
-
-    print("N: " + str(N))
-    print("E: " + str(E))
-    print("Public key: " + str(PUK))
-    print("D : " + str(D))
-    print("Private key: " + str(PIK))
-
-    Message = input("Enter a message to encrypt: ")
-    Message = Encrypt(PUK, Message)
-    print("Encrypted Message: " + str(Message))
-    Message = Decrypt(PIK, Message)
-    print("Decrypted Message: " + str(Message))
-    End_Application = input("")
+    print('RSA Encryption Algorithm [Version 1.0.2]\nWritten by Matthew Hahn\n')
+    while True:
+        UI = input(">>").split()
+        if UI[0].upper() == 'EXIT':
+           break
+        if UI[0].upper() == 'ENCRYPT' or UI[0].upper() == 'DECRYPT':
+            if len(UI) == 2 and UI[0].upper() == 'ENCRYPT':
+                try:
+                    myFile = open('Files/' + UI[1])
+                    P = GeneratePseudo(GeneratePrimes(),0,10000)
+                    Q = GeneratePseudo(GeneratePrimes(),0,10000)
+                    PUK = Public_Key(P, Q)
+                    E, N = PUK
+                    D = ModInv(E, ((P-1)*(Q-1)))
+                    PIK = D, N
+                    myFile.close()
+                    Encrypt_File(PUK, PIK, 'Files/' + UI[1])
+                except FileNotFoundError:
+                    print('\tError: "' + UI[1] + '" does not exist')
+            elif len(UI) == 2 and UI[0].upper() == 'DECRYPT':
+                try:
+                    myFile = open('Files/' + UI[1])
+                    var1, var2 = input("\tEnter Private Key ('D N'): ").split()
+                    PIK = int(var1), int(var2)
+                    myFile.close()
+                    Decrypt_File(PIK, 'Files/' + UI[1])
+                except FileNotFoundError:
+                    print('\tError: "' + UI[1] + '" does not exist')
+            elif len(UI) <= 1 or len(UI) >= 3:
+                print('\tError: Invalid parameter input')
+        else:
+            print('\tUnknown Command: "' + UI[0] + '"')
